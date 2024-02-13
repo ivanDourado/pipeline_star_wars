4dc490082a1dd175840dbfe920ffb5248062d033
ivanDourado <jaivan96@protonmail.com>
2024-02-13 15:44:09 -0300

    feat: definição de argumentos padrão por tarefa da dag

--- dags/star_wars_data_pipeline.py
+++ dags/star_wars_data_pipeline.py
@@ -12,3 +12,11 @@ sys.path.append('/opt/airflow/dags/utils')
 # Importa as funções dos scripts localizados no diretório utils.
 from script_request import main as main
 from script_results import generate_results_json as generate_results_json
+
+# Define argumentos padrão que serão passados para cada tarefa do DAG.
+default_args = {
+    'owner': 'airflow',  # Define o proprietário do DAG, geralmente 'airflow' ou o nome de usuário do criador do DAG.
+    'start_date': datetime.now() + timedelta(seconds=10) , # Define a data de início da execução do DAG.
+    'schedule_interval': None,
+    'retries': 5,  # Define o número de tentativas de reexecução em caso de falha.
+}
